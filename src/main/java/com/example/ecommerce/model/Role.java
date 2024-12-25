07d5dda7ba80a76914c80a45c1cfd39dddd24478
Fahd Khan <Fahdkhan487@gmail.com>
2024-12-25 15:26:49 +0500

    role issue

--- src/main/java/com/example/ecommerce/model/Role.java
+++ src/main/java/com/example/ecommerce/model/Role.java
@@ -4,6 +4,9 @@ public class Role {
     private int id;
     private String name;
 
+    public Role() {
+    }
+
     public Role(String name) {
         this.name = name;
     }
@@ -27,5 +30,13 @@ public class Role {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
