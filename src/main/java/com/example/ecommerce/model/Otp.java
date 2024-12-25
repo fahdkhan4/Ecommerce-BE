380776e50c22dcee46e8c7f9ad057206e4709b48
Abdul Rafay <rafayjawaid2004@gmail.com>
2024-12-25 12:43:18 +0500

    Added functionality to reset passward

---
+++ src/main/java/com/example/ecommerce/model/Otp.java
@@ -0,0 +1,41 @@
+package com.example.ecommerce.model;
+
+import java.time.LocalDateTime;
+
+public class Otp {
+    private String email;
+    private String otp;
+    private LocalDateTime expirationTime;
+
+    // Constructor
+    public Otp(String email, String otp, LocalDateTime expirationTime) {
+        this.email = email;
+        this.otp = otp;
+        this.expirationTime = expirationTime;
+    }
+
+    // Getters and setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOtp() {
+        return otp;
+    }
+
+    public void setOtp(String otp) {
+        this.otp = otp;
+    }
+
+    public LocalDateTime getExpirationTime() {
+        return expirationTime;
+    }
+
+    public void setExpirationTime(LocalDateTime expirationTime) {
+        this.expirationTime = expirationTime;
+    }
+}
